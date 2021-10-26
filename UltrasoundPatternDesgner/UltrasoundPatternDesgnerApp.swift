7360516380979af727896c9e04ecc8b1e8fbc6b4
YUAN-TSUNG <satoshi.liu@icloud.com>
2021-10-27 02:45:34 +0800

    Initial Commit

---
+++ UltrasoundPatternDesgner/UltrasoundPatternDesgnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltrasoundPatternDesgnerApp.swift
+//  UltrasoundPatternDesgner
+//
+//  Created by YUAN-TSUNG LIOU on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct UltrasoundPatternDesgnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
