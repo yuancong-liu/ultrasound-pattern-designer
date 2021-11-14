113d7580c9df2d69d3f75f178bf196bee150ea3e
YUAN-TSUNG <satoshi.liu@icloud.com>
2021-11-15 00:34:49 +0800

    Some cpp sample files added.

---
+++ UltrasoundPatternDesgner/Models/ShapeGenerationInterface.cpp
@@ -0,0 +1,19 @@
+//
+//  ShapeGenerationInterface.cpp
+//  UltrasoundPatternDesgner
+//
+//  Created by YUAN-TSUNG LIOU on 2021/11/15.
+//
+
+#include "ShapeGenerationInterface.hpp"
+
+#include <cmath>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+
+//I dont have this SDK
+//#include "UltrahapticsTimePointStreaming.hpp"
+
+
---
+++ UltrasoundPatternDesgner/Models/ShapeGenerationInterface.hpp
@@ -0,0 +1,13 @@
+//
+//  ShapeGenerationInterface.hpp
+//  UltrasoundPatternDesgner
+//
+//  Created by YUAN-TSUNG LIOU on 2021/11/15.
+//
+
+#ifndef ShapeGenerationInterface_hpp
+#define ShapeGenerationInterface_hpp
+
+#include <stdio.h>
+
+#endif /* ShapeGenerationInterface_hpp */
