b39d867ef0e24724e8cfea4c0eb10f88863186dd
YUAN-TSUNG <satoshi.liu@icloud.com>
2021-11-18 17:36:13 +0800

    Slight modifications.

--- UltrasoundPatternDesgner/Views/ObjectsViewRow.swift
+++ UltrasoundPatternDesgner/Views/ObjectsViewRow.swift
@@ -32,9 +32,9 @@ struct ObjectsViewRow: View {
             Text(modelData.currentShapes.count != 0 ? modelData.currentShapes[modelData.currentShapes.firstIndex(where: {
                 $0.id == self.userShape.id
             } ) ?? 0].name : "no")
-                .foregroundColor(modelData.currentShapes[modelData.currentShapes.firstIndex(where: {
+                .foregroundColor(modelData.problematicObjects.contains(modelData.currentShapes[modelData.currentShapes.firstIndex(where: {
                     $0.id == self.userShape.id
-                } ) ?? 0].problematic ? .pink : .black)
+                } ) ?? 0].id) ? .pink : .black)
             
             Spacer()
             
